d53c2acc848c868af8b55d4864ff3415b4fc7196
colin <cjaw426@gmail.com>
2026-01-07 20:39:19 -0600

    minor change, testing git push deploy on PA

--- django_weather_tracker/weather_tracker/serializers/weather_report_serializer.py
+++ django_weather_tracker/weather_tracker/serializers/weather_report_serializer.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from weather_tracker.models import WeatherReport
 
 
+
 class WeatherReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeatherReport
