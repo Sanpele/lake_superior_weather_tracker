8bcdb0817e659bafad3c740a6b4fb18f65f10563
colin <cjaw426@gmail.com>
2026-02-16 10:53:51 -0600

    first real unit test

--- django_weather_tracker/weather_tracker/tests/scraping_logic/test_gov_weather_request.py
+++ django_weather_tracker/weather_tracker/tests/scraping_logic/test_gov_weather_request.py
@@ -1,10 +1,35 @@
 from django.test import SimpleTestCase
+from unittest.mock import patch
+
+from rest_framework import status
+
+from weather_tracker.exceptions import ScraperError
+from weather_tracker.scraping_logic.weather_web_scraper import GovWeatherRequest
+from weather_tracker.tests.testing_mocks import valid_weather_api_response
 
 
 class TestGovWeatherRequest(SimpleTestCase):
 
-    def test_gov_weather_request__valid_request__parses_successfully(self):
-        pass
+    @patch(
+        "weather_tracker.scraping_logic.weather_web_scraper.get_request",
+        return_value=valid_weather_api_response,
+    )
+    def test_gov_weather_request__valid_request__parses_successfully(
+        self, _mock_get_request
+    ):
+        weather_dict = GovWeatherRequest().get()
 
-    def test_gov_weather_request__invalid_request__raises_scraper_error(self):
-        pass
+        self.assertTrue(isinstance(weather_dict, dict))
+        self.assertEqual(weather_dict["feed"]["author"]["name"], "Environment Canada")
+
+    @patch(
+        "weather_tracker.scraping_logic.weather_web_scraper.get_request",
+        side_effect=Exception("API ERROR"),
+    )
+    def test_gov_weather_request__invalid_request__raises_scraper_error(self, _):
+
+        with self.assertRaises(ScraperError) as e:
+            GovWeatherRequest().get()
+
+        self.assertTrue(isinstance(e.exception, ScraperError))
+        self.assertEqual(e.exception.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
---
+++ django_weather_tracker/weather_tracker/tests/testing_mocks.py
@@ -0,0 +1,91 @@
+valid_weather_api_response = """<?xml version="1.0" encoding="UTF-8"?>
+ <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-ca">
+ 
+<!--
+This is a data file that is meant to be read by RSS readers or aggregators.
+See https://www.canada.ca/en/environment-climate-change/services/weather-general-tools-resources/ways-to-access-forecasts.htmlto learn more about our RSS service.
+-->
+
+ <title>Lake Superior - Marine Weather - Environment Canada</title>
+ <link rel="related" href="https://www.weather.gc.ca/marine/index_e.html" type="text/html"/>
+ <link rel="self" href="https://www.weather.gc.ca/rss/marine/08500_e.xml" type="application/atom+xml"/>
+ <link rel="alternate" hreflang="fr-ca" href="https://www.meteo.gc.ca/rss/marine/08500_f.xml" type="application/atom+xml"/>
+ <author>
+ <name>Environment Canada</name>
+ <uri>https://www.weather.gc.ca</uri>
+ </author>
+ <updated>2026-02-16T15:26:57Z</updated>
+ <id>tag:weather.gc.ca,2013-04-16:20260216152657</id>
+ <logo>https://www.weather.gc.ca/template/gcweb/v15.0.0/assets/wmms-alt.png</logo>
+ <icon>https://www.weather.gc.ca/template/gcweb/v15.0.0/assets/favicon.ico</icon>
+ <rights>Copyright 2026, Environment Canada</rights>
+ <entry>
+ <title>Forecast for Today Tonight and Tuesday. - Eastern Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08503#forecast"/>
+ <updated>2026-02-16T15:30:00Z</updated>
+ <published>2026-02-16T15:30:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Wind south 15 knots diminishing to northwest 10 near noon then
+becoming light late overnight. Wind becoming east 10 Tuesday morning then
+increasing to east 20 Tuesday evening.  Scattered flurries or scattered showers ending this
+afternoon. Risk of freezing drizzle late this morning.      &#60;br/>Issued 10:30  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08503_regular_forecast:20260216153000</id>
+ </entry>
+ <entry>
+ <title>Waves for Today Tonight and Tuesday. - Eastern Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08503#forecast"/>
+ <updated>2026-02-16T15:30:00Z</updated>
+ <published>2026-02-16T15:30:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Over open water waves 1 metre subsiding to 0.5 this
+afternoon.&#60;br/>Issued 10:30  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08503_waves:20260216153000</id>
+ </entry>
+ <entry>
+ <title>Extended Forecast - Eastern Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08503#forecast"/>
+ <updated>2026-02-16T08:00:00Z</updated>
+ <published>2026-02-16T08:00:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Wednesday: Wind northeast 25 knots increasing to
+ northeast 35 in the morning then diminishing to east 30 late in the
+ day.&#60;br/>Thursday: Wind east 30 knots.&#60;br/>Friday: Wind east 20 knots.&#60;br/>Issued 03:00  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08503_extended_forecast:20260216080000</id>
+ </entry>
+ <entry>
+ <title>Forecast for Today Tonight and Tuesday. - Western Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08507#forecast"/>
+ <updated>2026-02-16T15:30:00Z</updated>
+ <published>2026-02-16T15:30:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Wind northwest 10 knots backing to west 10 this afternoon then
+veering to northwest 10 this evening. Wind becoming light early Tuesday morning
+then becoming east 10 near noon Tuesday. Wind increasing to east 20 Tuesday
+evening.  Scattered flurries or scattered showers this
+morning.      &#60;br/>Issued 10:30  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08507_regular_forecast:20260216153000</id>
+ </entry>
+ <entry>
+ <title>Waves for Today Tonight and Tuesday. - Western Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08507#forecast"/>
+ <updated>2026-02-16T15:30:00Z</updated>
+ <published>2026-02-16T15:30:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Over open water waves 0.5 to 1 metre subsiding to 0.5 or less
+near noon then building to 1 Tuesday evening.&#60;br/>Issued 10:30  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08507_waves:20260216153000</id>
+ </entry>
+ <entry>
+ <title>Extended Forecast - Western Lake Superior</title>
+ <link type="text/html" href="https://www.weather.gc.ca/marine/forecast_e.html?mapID=09&amp;siteID=08507#forecast"/>
+ <updated>2026-02-16T08:00:00Z</updated>
+ <published>2026-02-16T08:00:00Z</published>
+ <category term="Marine Forecasts"/>
+ <summary type="html">Wednesday: Wind northeast 25 knots increasing to
+ northeast 35 in the morning then diminishing to east 30 late in the
+ day.&#60;br/>Thursday: Wind east 30 knots.&#60;br/>Friday: Wind east 20 knots.&#60;br/>Issued 03:00  AM EST 16 February 2026</summary>
+ <id>tag:weather.gc.ca,2013-04-16:08507_extended_forecast:20260216080000</id>
+ </entry>
+ </feed>
+
+"""
